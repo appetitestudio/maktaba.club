4ea1b101199b45da0f3501867f83498838c3d48e
Muhammed Ameen <ameens.in19@gmail.com>
2024-10-07 21:07:39 +0530

    🔧Config: Routes added

--- src/app/eth/page.tsx
+++ src/app/eth/page.tsx
@@ -1,7 +1,7 @@
-// Add this directive at the top
 "use client"; 
 
 import React, { useEffect } from 'react';
+import Head from 'next/head'; // Import Head for metadata
 
 function Eth() {
   useEffect(() => {
@@ -9,7 +9,18 @@ function Eth() {
     window.location.href = 'https://appetitestudio.notion.site/Ethereum-Developer-Bootcamp-915187c6fa1e483daf9563b1af035f4e?pvs=74';
   }, []); // This runs only once when the component mounts
 
-  return null; // No need to render anything
+  return (
+    <>
+      <Head>
+        <title>Ethereum Developer Bootcamp - Maktaba Club</title>
+        <meta name="description" content="Join the Ethereum Developer Bootcamp to fast-track your career in Web3 with hands-on projects, expert mentorship, and a dynamic community." />
+        <meta property="og:title" content="Ethereum Developer Bootcamp - Maktaba Club" />
+        <meta property="og:description" content="Fast-track your career in Web3 development by joining our Ethereum Bootcamp with hands-on projects and expert mentorship." />
+        <meta property="og:url" content="https://appetitestudio.notion.site/Ethereum-Developer-Bootcamp-915187c6fa1e483daf9563b1af035f4e?pvs=74" />
+      </Head>
+      {/* No need for visible content since it's redirecting */}
+    </>
+  );
 }
 
 export default Eth;
